cefabd5d58ed04bf909f1f787854b52c6d8c43b2
KingJoJoXZ <echolsn@go.stockton.edu>
2023-11-03 11:06:56 -0400

    Updated name files for nodecg

---
+++ dashboard/name.js
@@ -0,0 +1,7 @@
+const nameInput = document.getElementById('name')
+const nickInput = document.getElementById('Nickname')
+
+function update(){
+    const data = {name:nameInput.value, Nickname: nickInput.value}
+   // nodecg.sendMessage('showLowerThird', data);
+}
