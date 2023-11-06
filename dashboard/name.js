6d0e9dbc3848a712ad21723e2a579caa75cc18c3
KingJoJoXZ <echolsn@go.stockton.edu>
2023-11-06 18:53:09 -0500

    Updated .gitignore Corrected name.html & name.js Corrected score.css Added html files to package.json

--- dashboard/name.js
+++ dashboard/name.js
@@ -3,5 +3,5 @@ const nickInput = document.getElementById('Nickname')
 
 function update(){
     const data = {name:nameInput.value, Nickname: nickInput.value}
-   // nodecg.sendMessage('showLowerThird', data);
+    nodecg.sendMessage('showLowerThird', data);
 }
