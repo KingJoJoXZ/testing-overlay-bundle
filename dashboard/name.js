c67d4184913b9a6e3d31d21ff5e2a78351e5a634
KingJoJoXZ <145947747+KingJoJoXZ@users.noreply.github.com>
2023-11-10 21:11:48 -0500

    Delete dashboard/name.js

--- dashboard/name.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.getElementById('name')
-const nickInput = document.getElementById('nickname')
-
-function update(){
-    const data = {name: nameInput.value, nickname: nickInput.value}
-    nodecg.sendMessage('showLowerThird', data);
-}
